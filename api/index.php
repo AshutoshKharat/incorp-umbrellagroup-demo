7ff0f8cdcf1bef3d6be5f2fe1a869ba870a7baad
AshutoshKharat <ashutoshkharat41@gmail.com>
2026-04-21 01:13:14 +0530

    Added links

--- api/index.php
+++ api/index.php
@@ -1,1 +1,27 @@
-<?php require_once __DIR__ . '/../index.php'; ?>
+<?php
+$requestPath = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
+$normalizedPath = rtrim((string) $requestPath, '/');
+
+if ($normalizedPath === '') {
+	$normalizedPath = '/';
+}
+
+$routeMap = [
+	'/' => 'index.php',
+	'/index.php' => 'index.php',
+	'/about' => 'about.php',
+	'/about.php' => 'about.php',
+	'/team' => 'team.php',
+	'/team.php' => 'team.php',
+];
+
+$targetFile = $routeMap[$normalizedPath] ?? null;
+
+if ($targetFile === null) {
+	http_response_code(404);
+	echo 'Page not found';
+	exit;
+}
+
+require_once __DIR__ . '/../' . $targetFile;
+?>
